fd3ec1c993d19e34d8ec2bf00a2526c1eb8017d0
Ali Mir <alisafeer2672@gmail.com>
2026-01-18 17:11:10 +0500

    Add files via upload

---
+++ Lab_task_01/Task 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		cout<<"************************************************"<<endl;
+	cout<<"\tName:\t\tAli Safeer\t\t"<<endl;
+	cout<<"\tRoll No:\t2025-SET-46\t\t\t"<<endl;
+	cout<<"\tDepartment:\tSoftware Engineering\t"<<endl;
+	cout<<"\tAddress:\tChella Bandi,Muzaffarabad\t"<<endl;
+	cout<<"\tHobby:\t\tPlaying Football\t\t\t"<<endl;
+	cout<<"\tQualification:\tFsc\t\t\t"<<endl;
+	cout<<"\tSSC Marks:\t860\t\t\t"<<endl;
+	cout<<"\tHSSC Marks:\t660\t\t\t"<<endl;
+	cout<<"************************************************"<<endl;
+	return 0;
+
+}
